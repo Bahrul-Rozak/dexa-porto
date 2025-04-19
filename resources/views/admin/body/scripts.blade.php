2bcdb514ce815e0d69fc598f66a6c4428ec6f9b9
bahrulrozak <bahrulrozak02@gmail.com>
2025-04-19 07:57:27 +0700

    Integrated Template With Love

---
+++ resources/views/admin/body/scripts.blade.php
@@ -0,0 +1,17 @@
+<!-- Required vendors -->
+<script src="{{ asset('backend/vendor/global/global.min.js') }}"></script>
+<script src="{{ asset('backend/vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+<script src="{{ asset('backend/vendor/chart.js/Chart.bundle.min.js') }}"></script>
+
+<!-- Chart piety plugin files -->
+<script src="{{ asset('backend/vendor/peity/jquery.peity.min.js') }}"></script>
+
+<!-- Apex Chart -->
+<script src="{{ asset('backend/vendor/apexchart/apexchart.js') }}"></script>
+
+<!-- Dashboard 1 -->
+<script src="{{ asset('backend/js/dashboard/dashboard-1.js') }}"></script>
+
+<script src="{{ asset('backend/vendor/owl-carousel/owl.carousel.js') }}"></script>
+<script src="{{ asset('backend/js/custom.min.js') }}"></script>
+<script src="{{ asset('backend/js/deznav-init.js') }}"></script>
---
+++ resources/views/admin/body/styles.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="{{ asset('backend/vendor/chartist/css/chartist.min.css') }}">
+<link href="{{ asset('backend/vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet">
+<link href="{{ asset('backend/vendor/owl-carousel/owl.carousel.css') }}" rel="stylesheet">
+<link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
